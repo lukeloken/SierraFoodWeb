42414407dca993c9d27ed0dea330588302864687
Luke <lukeloken@gmail.com>
2019-01-16 21:54:36 -0800

    added additional trophic levels

--- R/CleanIsotopeData.R
+++ R/CleanIsotopeData.R
@@ -32,28 +32,67 @@ plot(IsotopeAll$Elevation2, IsotopeAll$"δ13C ‰ vs VPD")
 points(IsotopeAll$Elevation, IsotopeAll$"δ13C ‰ vs VPD", col='red')
 
 
-IsotopeSub<-IsotopeAll[IsotopeAll$`Sample type` %in% c('Zooplankton', 'DIC', 'Sediment abyssal', 'POM', 'DOC', 'Terrestrial veg'),]
+IsotopeSub<-IsotopeAll[IsotopeAll$`Sample type` %in% c('Zooplankton', 'DIC', 'Sediment abyssal', 'POM', 'DOC', 'Terrestrial veg', 'Fish', 'SAP', 'EAP', 'Aquatic Insect', 'Benthic Infauna'),]
 
 
 
-IsotopeSub$Group<-factor(IsotopeSub$`Sample type`)
+IsotopeSub$Group<-factor(IsotopeSub$`Sample type`, c('Fish',  'Zooplankton', 'Aquatic Insect', 'Benthic Infauna','Sediment abyssal', 'POM', 'DOC', 'Terrestrial veg',  'SAP', 'EAP', 'DIC'))
 IsotopeSub$Depth<-IsotopeSub$`Max Depth`
 IsotopeSub$delC<-IsotopeSub$"δ13C ‰ vs VPD"
+IsotopeSub$delN<-IsotopeSub$"δ15N ‰ vs Air-N2"
+IsotopeSub$delH<-IsotopeSub$"δ2H\r\n(per mil vs. VSMOW)"
+
 
 color.palette = colorRampPalette(c(viridis(6, begin=.2, end=.98), rev(magma(5, begin=.35, end=.98))), bias=1)
-colorset<-'Dark2'
-colors<-brewer.pal(length(unique(IsotopeSub$Group)), colorset)
+colorset<-'Paired'
+colors<-c(brewer.pal(length(unique(IsotopeSub$Group))-1, colorset), 'black')
 
 
 # colors<-color.palette(length(unique(merge_df$Station)))
+
+png(paste0(box_dir, '/Figures/delC_Elevation.png'), units='in', width=7, height=7, res=400, bg='white')
 
 
 ggplot(IsotopeSub, aes_string("Elevation", "delC", group="Group")) + 
   scale_shape_manual(values=rep(21:25, 5))  + 
   scale_fill_manual(values = colors) + 
   scale_colour_manual(values = colors) +
-  geom_jitter(size=2, width=20, aes(fill=Group, shape=Group)) + 
+  geom_smooth(method='lm', alpha=0.2, aes(fill=Group, colour=Group)) +
+  geom_jitter(size=2, width=40, aes(fill=Group, shape=Group)) + 
   theme_bw() +
   theme(plot.title = element_text(hjust=0.5))  +
   theme(legend.position='bottom')
 
+dev.off()
+
+#Nitrogen
+png(paste0(box_dir, '/Figures/delN_Elevation.png'), units='in', width=7, height=7, res=400, bg='white')
+
+
+ggplot(IsotopeSub[is.finite(IsotopeSub$delN),], aes_string("Elevation", "delN", group="Group")) + 
+  scale_shape_manual(values=rep(21:25, 5))  + 
+  scale_fill_manual(values = colors) + 
+  scale_colour_manual(values = colors) +
+  geom_smooth(method='lm', alpha=0.2, aes(fill=Group, colour=Group)) +
+  geom_jitter(size=2, width=40, aes(fill=Group, shape=Group)) + 
+  theme_bw() +
+  theme(plot.title = element_text(hjust=0.5))  +
+  theme(legend.position='bottom')
+
+dev.off()
+
+#Hydrogen
+png(paste0(box_dir, '/Figures/delH_Elevation.png'), units='in', width=7, height=7, res=400, bg='white')
+
+
+ggplot(IsotopeSub[is.finite(IsotopeSub$delH),], aes_string("Elevation", "delH", group="Group")) + 
+  scale_shape_manual(values=rep(21:25, 5))  + 
+  scale_fill_manual(values = colors) + 
+  scale_colour_manual(values = colors) +
+  geom_smooth(method='lm', alpha=0.2, aes(color=Group, fill=Group)) +
+  geom_jitter(size=2, width=40, aes(fill=Group, shape=Group)) + 
+  theme_bw() +
+  theme(plot.title = element_text(hjust=0.5))  +
+  theme(legend.position='bottom')
+
+dev.off()
