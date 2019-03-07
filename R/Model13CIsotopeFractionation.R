1d6ceef9daf92b87c4722c14d929d18093f79512
Luke <lukeloken@gmail.com>
2019-03-07 08:23:29 -0800

    updated figure for phytoplankton expectation based on dic chemistry. uses 'Money' column from Simone's excel spreadsheet

--- R/Model13CIsotopeFractionation.R
+++ R/Model13CIsotopeFractionation.R
@@ -1,3 +1,14 @@
+
+
+
+blue.trans <- adjustcolor(colors()[62],alpha.f=0.6)
+green.trans <- adjustcolor('#006837', alpha.f=0.6)
+aquatic.trans <- adjustcolor('#c2e699', alpha.f=0.6)
+brown.trans <- adjustcolor('brown', alpha.f=0.6)
+gray.trans <- adjustcolor('gray', alpha.f=0.7)
+black.trans <- adjustcolor('black',alpha.f=0.7)
+
+
 # Isotope Fractionation Model
 
 #Model Fractionaction of Daphnia galeata
@@ -42,32 +53,152 @@ CIsotopeSummary<- IsotopeSub %>%
 LakeSummary$Ep<-Ep_Smyntek(LakeSummary$`CO2 uM`)
 LakeSummary$Lake = LakeSummary$'Lake Name'
 
-LakeSummary$Phyto_ExpectedD13<-LakeSummary$'d13C DIC'-LakeSummary$Ep
+LakeSummary$Phyto_ExpectedD13<-LakeSummary$Money-LakeSummary$Ep
 
 LakeSummary2<-full_join(LakeSummary, CIsotopeSummary)
 
-LakeSummary2$PhytoExpected_Epi<-LakeSummary2$DIC-LakeSummary2$Ep
+LakeSummary2$PhytoExpected_Epi<-LakeSummary2$Money-LakeSummary2$Ep
 
 # LakeSummary$Sediment_ObservedD13<-IsotopeSub$"δ13C ‰ vs VPD"[match(LakeSummary$`Lake Name`, IsotopeSub$Lake)]
 
 
-png(paste0(box_dir, '/Figures/Expected_delC_Phyto.png'), units='in', width=5, height=4, res=400, bg='white')
-par(mar=c(3,3,.5,.5), mgp=c(2,.3,0), tck=-.01)
-plot(LakeSummary2$`CO2 uM`, LakeSummary2$'d13C DIC', col='black', pch=2, ylim=c(-38,5), ylab='', xlab='', las=1)
-arrows(x0=LakeSummary2$`CO2 uM`, y0=LakeSummary2$'d13C DIC', y1=LakeSummary2$PhytoExpected_Epi, length=0.1, lty=1, col='grey')
-# 
+yvars <- c('del13C')
+xvar <- 'CO2 uM'
+
+ylims <- c(-38, 5)
+xlims <- c(0,50)
+xaxint <- 400
+
+LakeSummary2$CO2uM<-LakeSummary2$`CO2 uM`
+
+
+co2_x= data.frame(CO2uM=seq(-10, 50,1))
+
+Ter_lm<-lm(`Terrestrial veg` ~ CO2uM, data=LakeSummary2 )
+Ter_pred<-stats::predict(Ter_lm,newdata=co2_x, interval='confidence', level=0.95)
+
+Phyto_lm<-lm(PhytoExpected_Epi ~ CO2uM, data=LakeSummary2 )
+Phyto_pred<-stats::predict(Phyto_lm,newdata=co2_x, interval='confidence', level=0.95)
+
+POM_lm<-lm(POM ~ CO2uM, data=LakeSummary2 )
+POM_pred<-stats::predict(POM_lm,newdata=co2_x, interval='confidence', level=0.95)
+
+DIC_lm<-lm(`d13C DIC` ~ CO2uM, data=LakeSummary2 )
+DIC_pred<-stats::predict(DIC_lm,newdata=co2_x, interval='confidence', level=0.95)
+
+Money_lm<-lm(Money ~ CO2uM, data=LakeSummary2 )
+Money_pred<-stats::predict(Money_lm,newdata=co2_x, interval='confidence', level=0.95)
+
+
+terr_veg <- c('Tree_Pinaceae', 'Tree non-pine', 'Grass','Shrub','Soil')#sample types lumped together as terr veg
+aq_veg <- c('SAP','EAP','Wetland plants')
+
+terrveg.sd<-sd(LakeSummary2$`Terrestrial veg`,na.rm=TRUE)
+pom.sd<-sd(LakeSummary2$POM,na.rm=TRUE)
+dic.sd<-sd(LakeSummary2$DIC,na.rm=TRUE)
+money.sd<-sd(LakeSummary2$Money,na.rm=TRUE)
+
+
+
+png(paste0(box_dir, '/Figures/FoodWebPaper_Figure3_d13C.png'), units='in', width=5.5, height=10, res=300, bg='white')
+
+par(mfrow=c(3,1),mar=c(3.5,3,1,0.5),oma=c(2,2,1,1), xpd=F)
+
+#plot 1
+
+plot(LakeSummary2$Elevation, LakeSummary2$`CO2 uM`, ylab='', xlab='', las=1, col='black', cex=1.4, axes=F,pch=16)
+abline(lm( LakeSummary2$`CO2 uM` ~ LakeSummary2$Elevation ), lwd=2)
+
+axis(side=2,lwd=2,las=2, cex.axis=1.2,col.axis='black')
+box(lwd=2)
+mtext(side=1, line=2.7, expression(paste("Elevation (m)")), cex=1.2)
+axis(side=1, lwd=2, cex.axis=1.2,col.axis='black')
+mtext(side=2, line=2.9, expression(paste(CO[2], " (", mu, "M)")), cex=1.2)
+
+#plot 2
+
+plot(LakeSummary2$`CO2 uM`, LakeSummary2$Ep, ylab='', xlab='', las=1, col='black', type='p', cex=1.4, axes=F, pch=16)
+lines(seq(0.01, 50, 0.1), Ep_Smyntek(seq(0.01, 50, 0.1)))
+
+
+axis(side=2,lwd=2,las=2, cex.axis=1.2,col.axis='black')
+box(lwd=2)
+mtext(side=2, line=2.9, expression(paste(epsilon[p], ' (', "\u2030", ')', sep='')), cex=1.2)
+axis(side=1, lwd=2, cex.axis=1.2,col.axis='black')
+mtext(side=1, line=2.7, expression(paste(CO[2], " (", mu, "M)")), cex=1.2)
+
+
+#plot 3
+
+plot(LakeSummary2$`CO2 uM`, LakeSummary2$'d13C DIC', ylim=c(-38,-5), ylab='', xlab='', las=1, col=blue.trans, type='n', cex=1.4, axes=F)
+
+polygon(x=c(co2_x[,1],rev(co2_x[,1])),y=c((Ter_pred[,1]-terrveg.sd), rev((Ter_pred[,1]+terrveg.sd))),col=green.trans,border=NA)
+# polygon(x=c(co2_x[,1],rev(co2_x[,1])),y=c((DIC_pred[,1]-dic.sd), rev((DIC_pred[,1]+dic.sd))),col=gray.trans,border=NA)
+polygon(x=c(co2_x[,1],rev(co2_x[,1])),y=c((Money_pred[,1]-money.sd), rev((Money_pred[,1]+money.sd))),col=gray.trans,border=NA)
+polygon(x=c(co2_x[,1],rev(co2_x[,1])),y=c((Phyto_pred[,1]-2), rev((Phyto_pred[,1]+3))),col=aquatic.trans,border=NA)
+
+
+abline(lm( LakeSummary2$'Terrestrial veg' ~ LakeSummary2$`CO2 uM` ), col=green.trans, lwd=2)
+# abline(lm( LakeSummary2$'d13C DIC' ~ LakeSummary2$`CO2 uM` ), col=blue.trans, lwd=2)
+abline(lm( LakeSummary2$Money ~ LakeSummary2$`CO2 uM` ), col=gray.trans, lwd=2)
+abline(lm( LakeSummary2$PhytoExpected_Epi ~ LakeSummary2$`CO2 uM` ), col=aquatic.trans, lwd=2)
+abline(lm( LakeSummary2$POM ~ LakeSummary2$`CO2 uM` ), col="black", lwd=2)
+
+x_arrow<-50
+arrows(x0=co2_x[x_arrow,1], y0=Money_pred[x_arrow,1], y1=Phyto_pred[x_arrow,1], length=0.1, lty=1, col='black')
+text(co2_x[x_arrow,1], mean(c(Money_pred[x_arrow,1], Phyto_pred[x_arrow,1])), expression(paste(epsilon[p])), pos=4, cex=2)
 # points(LakeSummary2$`CO2 uM`, LakeSummary2$DOC, bg='orange', pch=22)
 # points(LakeSummary2$`CO2 uM`, LakeSummary2$`Sediment abyssal`, bg='brown', pch=23)
 # points(LakeSummary2$`CO2 uM`, LakeSummary2$Zooplankton, bg='purple', pch=24)
-points(LakeSummary2$`CO2 uM`, LakeSummary2$PhytoExpected_Epi, pch=8, col='darkgreen')
-points(LakeSummary2$`CO2 uM`, LakeSummary2$POM, bg='green3', pch=21)
+# points(LakeSummary2$`CO2 uM`, LakeSummary2$'d13C DIC', col=blue.trans, pch=16, cex=1.4)
 
-legend('topleft', inset=0.02, c('Epilimnion DIC', 'Expected Phyto', 'Observed POM'), pch=c(2,8,21), col=c('black', 'darkgreen', 'black'), pt.bg=c('green1'), bty='n', ncol=2)
+points(LakeSummary2$`CO2 uM`, LakeSummary2$Money, col=gray.trans, pch=16, cex=1.4)
+
+points(LakeSummary2$`CO2 uM`, LakeSummary2$PhytoExpected_Epi, pch=21, cex=1.4, col=green.trans)
+points(LakeSummary2$`CO2 uM`, LakeSummary2$'Terrestrial veg', pch=16, cex=1.4, col=green.trans)
+points(LakeSummary2$`CO2 uM`, LakeSummary2$POM, col='black', pch=16, cex=1.4)
+
+
+legend('topleft', legend=c('CO2','Terr Veg','Phyto','POM'),pch=21,col=c(gray.trans,green.trans,green.trans,black.trans),pt.bg=c(gray.trans,green.trans,aquatic.trans,black.trans), cex=1.1 ,horiz=T)
+
+
+# legend('topleft', inset=0.02, c('Epilimnion DIC', 'Expected Phyto', 'Observed POM'), pch=c(2,8,21), col=c('black', 'darkgreen', 'black'), pt.bg=c('green1'), bty='n', ncol=2)
+# 
+
+axis(side=2,lwd=2,las=2, cex.axis=1.2,col.axis='black')
+box(lwd=2)
+mtext(side=2, line=2.9, expression(paste(delta^{13}, "C Resource (\u2030)")), cex=1.2)
+axis(side=1, lwd=2, cex.axis=1.2,col.axis='black')
+mtext(side=1, line=2.7, expression(paste(CO[2], " (", mu, "M)")), cex=1.2)
+
+
+dev.off()
+
+
+
+
+# plot(LakeSummary2$`CO2 uM`, LakeSummary2$'d13C DIC', col='black', pch=2, ylim=c(-38,5), ylab='', xlab='', las=1, type='n')
+
+plot(IsotopeSub[IsotopeSub$'Sample description' %in% terr_veg,xvar],IsotopeSub[IsotopeSub$'Sample description' %in% terr_veg,yvars[1]], pch=NA,xlim=xlims,ylim=ylims, 
+     col=green.trans,bg=green.trans,cex=1.3,yaxs='i',xaxs='i', axes=F,ann=F)
+polygon(x=c(pred.phyto$Elevation,rev(pred.phyto$Elevation)), y=c(veg_elev_preds[,1]-terrveg.sd,rev(veg_elev_preds[,1]+terrveg.sd)),col=green.trans,border=NA)
+abline(veg_elev.lm,lwd=2,col=green.trans)
+#Predicted phyto signature (subtract Eh from d2H-H2O, plus or minus 1 sd for EH)
+polygon(x=c(pred.phyto$Elevation,rev(pred.phyto$Elevation)), y=c(pred.d2H[,1]-177, rev(pred.d2H[,1])-123),col=aquatic.trans,border=NA)
+abline( predphyto.lm, lwd=2, col=aquatic.trans)
+abline(pom_elev.lm,lwd=2,col=black.trans)
+
+axis(1)
+axis(2)
 
 mtext(expression(paste(CO[2], ' (', mu, 'M)', sep='')), 1, 1.5)
 mtext(expression(paste(delta, ''^'13', 'C (', "\u2030", ')', sep='')), 2, 1.5)
 
+
 dev.off()
+
+
+
 
 
 png(paste0(box_dir, '/Figures/Expected_delC_Phyto_withgroups.png'), units='in', width=5, height=4, res=400, bg='white')
